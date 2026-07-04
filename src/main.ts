cb7583bb3c742d9f3d0c32c495a21769af049333
saki <sakihet@gmail.com>
2026-07-04 12:02:14 +0900

    feat: add google analytics via vue-gtag

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
+import { createGtag } from 'vue-gtag'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(createGtag({ tagId: 'G-NR9ZJ7PEN9' }))
 
 app.mount('#app')
